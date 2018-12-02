b0c27ab3436b6b18134b67f6314da6c4d2557bb8
olu sogbesan <sogbesano@gmail.com>
2018-12-02 13:37:02 +0000

    initial commit

---
+++ src/day_one.rs
@@ -0,0 +1,31 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn read_file(path: String) -> std::vec::Vec<String> {
+  let mut file = File::open(path).expect("file not found");
+  let mut contents = String::new();
+  file.read_to_string(&mut contents)
+      .expect("something went wrong reading the file");
+  let contents_split = contents.split("\n");
+  let vec_contents: Vec<String> = contents_split.map(|s| s.to_string()).collect(); 
+  return vec_contents;
+}
+
+fn main() {
+  let vec_nums_strs = read_file(String::from("input.txt"));
+  let vec_nums: Vec<i32> = vec_nums_strs.iter().map(|s| if s.starts_with('-') { 
+                                     let number_str: String = s.chars().skip(1).take(s.chars().count()).collect();
+                                     let number: i32 = number_str.parse().unwrap();
+                                     return number * -1;
+                                 } else {
+                                     let number_str: String = s.chars().skip(1).take(s.chars().count()).collect();
+                                     let number: i32 = number_str.parse().unwrap();
+                                     return number;
+                                 }
+                                 ).collect();
+  for i in &vec_nums {
+    println!("{}", i);
+  }
+  let sum = vec_nums.iter().fold(0, |a, &b| a + b);
+  println!("the sum is {}", sum);
+}
